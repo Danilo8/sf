a8ebc8ddd935ad13c6584b24bd84e10557f8addd
Danilo8 <danilodesenvolvedorweb@hotmail.com>
2018-09-06 15:16:30 -0300

    Adicionado DB_connection e settings.php

---
+++ config/settings.php
@@ -0,0 +1,12 @@
+<?php
+
+error_reporting(E_ALL ^ E_NOTICE);
+ini_set('display_errors', 1);
+
+define('HOST', 'localhost');    //definindo o Servidor
+define('USER', 'root');         //definindo o Usuário
+define('PASSWORD', '');         //definindo a Senha
+define('DB', '');               //definindo o Banco de Dados 
+
+//Definindo o Timezone do PHP para o horário do Brasil
+date_default_timezone_set('America/Sao_Paulo');
---
+++ database/DB/DB_connection.php
@@ -0,0 +1,21 @@
+<?php
+
+//Busca o arquivo settings.php na pasta config
+require_once 'config/settings.php';
+
+class DB_connection {
+
+    private $mysqli;
+
+    public function __construct() {
+        $this->mysqli = new mysqli(HOST, USER, PASSWORD, DB);
+        if ($this->mysqli->connect_errno) {
+            die("Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error);
+        }
+    }
+
+    public function getConnection() {
+        return $this->mysqli;
+    }
+
+}
